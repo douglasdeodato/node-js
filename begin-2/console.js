24a3d170bca3420d5ad78a4151f1c6bfe6576c51
Douglas Deodato <douglasdeodato@gmail.com>
2012-01-25 00:24:51 +0000

    insert response.end

--- begin-2/console.js
+++ begin-2/console.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(request, response){
-	console.log("Usuário chamou a url ["+ request.url + "]");	
+	console.log("Usuário chamou a url ["+ request.url + "]");
+	response.end("você chamou a url:" + request.url);	
 });
 server.listen(8000);
 console.log("Servidor iniciado em http://localhost:8000");
