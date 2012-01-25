990d422785c9d4921ecfb0923b789c665697b6f1
Douglas Deodato <douglasdeodato@gmail.com>
2012-01-25 00:11:57 +0000

    nothing more checking code.

--- begin-1/server.js
+++ begin-1/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server =http.createServer(function(request, response) {	
+var server = http.createServer(function(request, response) {	
 	response.end("funcionou");	
 });
 server.listen(8000);
